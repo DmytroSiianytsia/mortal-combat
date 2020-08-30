e256e2278f21078e907dd4b12f564cfe27c32dda
Dima <ds081181@gmail.com>
2020-08-30 23:28:05 +0300

    Updates

---
+++ precache-manifest.7c241a46a931bcffe523f1b1b6e717ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ebedc933545519dc8293b5b84af132",
+    "url": "/mortal-combat/index.html"
+  },
+  {
+    "revision": "29cb05f858451056f509",
+    "url": "/mortal-combat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fce174411851be8d81d",
+    "url": "/mortal-combat/static/js/2.4b89da97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mortal-combat/static/js/2.4b89da97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cb05f858451056f509",
+    "url": "/mortal-combat/static/js/main.28ff4d9b.chunk.js"
+  },
+  {
+    "revision": "f6bbcf16c649eb585494",
+    "url": "/mortal-combat/static/js/runtime-main.aa5e457c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mortal-combat/static/media/logo.5d5d9eef.svg"
+  }
+]);
